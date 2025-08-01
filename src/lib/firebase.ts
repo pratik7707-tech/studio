aff8af098187b28ea81e4cd796982b576a70befc
Pratik <pratikshendkar7707@gmail.com>
2025-08-01 10:47:52 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  "projectId": "budgetwise-tp3pe",
+  "appId": "1:943872607880:web:66ee805b18066841f1c13d",
+  "storageBucket": "budgetwise-tp3pe.firebasestorage.app",
+  "apiKey": "AIzaSyA_zB2haKA_5ccUuRLBzmDzfoEgkr1ge8w",
+  "authDomain": "budgetwise-tp3pe.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "943872607880"
 };
 
 // Initialize Firebase
